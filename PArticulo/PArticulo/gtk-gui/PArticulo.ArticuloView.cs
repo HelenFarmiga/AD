17d5d6b46be9dacf1e008572bf31ca3d4b83894a
Elena Navarrete Contreras <elelenacon@gmail.com>
2015-10-29 09:10:34 +0100

    Añadido PSerpisAd con todo correcto

---
+++ PArticulo/PArticulo/gtk-gui/PArticulo.ArticuloView.cs
@@ -0,0 +1,9 @@
+
+namespace PArticulo
+{
+	public partial class ArticuloView
+	{
+		private Gtk.Button buttonCancel;
+		private Gtk.Button buttonOk;
+	}
+}
--- PSerpisAd/PSerpisAd/MainWindow.cs
+++ PSerpisAd/PSerpisAd/MainWindow.cs
@@ -1,20 +1,11 @@
 using System;
 using Gtk;
 
-using SerpisAd;
-
 public partial class MainWindow: Gtk.Window
 {	
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
-		Console.WriteLine ("MainWindow ctor.");
-		QueryResult queryResult = PersisterHelper.Get ("select * from articulo");
-		TreeViewHelper.Fill (treeView, queryResult);
-
-		newAction.Activated += delegate {
-			new ArticuloView ();
-		};
 	}
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
