d83866e4702d5bc54173a511b563501be60bbfb9
Elena Navarrete Contreras <elelenacon@gmail.com>
2015-12-17 10:41:20 +0100

    Subiendo mejoras

--- GMySql/src/main/java/org/institutoserpis/ad/PruebaArticulo.java
+++ GMySql/src/main/java/org/institutoserpis/ad/PruebaArticulo.java
@@ -5,8 +5,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
-
+import java.sql.ResultSetMetaData;
 
 public class PruebaArticulo {
 	
@@ -16,24 +15,39 @@ public class PruebaArticulo {
 				"jdbc:mysql://localhost/dbprueba","root","sistemas");
 		Statement statement= connection.createStatement();
 		ResultSet resultSet = statement.executeQuery("SELECT * FROM articulo");
+		ArrayList<String> nombres = new ArrayList<String>();
+		ResultSetMetaData meta = resulset.getMetaData();
 		
-		while (resultSet.next() ) {
-			   int Id= resultSet.getInt("id");
-			   String Nombre= resultSet.getString("nombre");
-			   int Categoria= resultSet.getInt("categoria");
-			   String Precio= resultSet.getString("precio");
-			   
-			   System.out.println(  "Id: "+ Id +"\n" + 
-					   				"Nombre: "+Nombre+
-					   			  
-			   					   " Categoria: " + Categoria +
-			   					   " Precio: "    + Precio);
+		int colum=meta.getColumnCount();
+		
+		for(int i = 1; i<=colum;i++){
+			nombres.add(meta.getColumnName(i));
+		}
+		
+		while(resulset.next()){
+			ArrayList<Object> lista = new ArrayList<Object>();
+			for(int i = 0; i<colum;i++){
+				lista.add(resulset.getObject(nombres.get(i)));
 			}
+			for(int i = 0; i<colum;i++){
+			System.out.print(nombres.get(i)+" : "+lista.get(i).toString()+"\t");
+			}
+			System.out.println();
+		}
+				
 		connection.close();
 		System.out.println("fin");
 	}			
 	
+	public static void showColumns(Connection connection, Statement statement) throws SQLException {
+		ResultSet resultSet = statement.executeQuery("SELECT  id, nombre, categoria, precio FROM articulo");
+		ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+		int numColumnas = resultSetMetaData.getColumnCount();
 
+		for (int i=1; i <= numColumnas; i++ ) {
+		  String nombreColumna = resultSetMetaData.getColumnName(i);
+		  System.out.println(nombreColumna);
+		}
 			}
 		
 			 
