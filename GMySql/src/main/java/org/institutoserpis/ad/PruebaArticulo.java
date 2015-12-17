f7223ea2ae1ad71714fba2e2bcc5735ec747b297
Elena Navarrete Contreras <elelenacon@gmail.com>
2015-12-17 09:32:49 +0100

    Subiendo mejoras en GMySql

--- GMySql/src/main/java/org/institutoserpis/ad/PruebaArticulo.java
+++ GMySql/src/main/java/org/institutoserpis/ad/PruebaArticulo.java
@@ -4,8 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-import com.mysql.jdbc.Statement;
+
 
 public class PruebaArticulo {
 	
@@ -13,17 +14,28 @@ public class PruebaArticulo {
 		
 		Connection connection = DriverManager.getConnection(
 				"jdbc:mysql://localhost/dbprueba","root","sistemas");
+		Statement statement= connection.createStatement();
+		ResultSet resultSet = statement.executeQuery("SELECT * FROM articulo");
+		
+		while (resultSet.next() ) {
+			   int Id= resultSet.getInt("id");
+			   String Nombre= resultSet.getString("nombre");
+			   int Categoria= resultSet.getInt("categoria");
+			   String Precio= resultSet.getString("precio");
+			   
+			   System.out.println(  "Id: "+ Id +"\n" + 
+					   				"Nombre: "+Nombre+
+					   			  
+			   					   " Categoria: " + Categoria +
+			   					   " Precio: "    + Precio);
+			}
 		connection.close();
 		System.out.println("fin");
 	}			
 	
 
-		public  void MostarArticulo (Connection connection, Statement statement) throws SQLException {
-			String instruccion = "SELECT * FROM articulo";
-			ResultSet resultSet = statement.executeQuery(instruccion);
-		}
-	}
-			 
+			}
+		
 			 
 			 
 			 
