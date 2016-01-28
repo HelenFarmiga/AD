12baa10156240756baa5d80acd5b47bfbd79c412
Elena Navarrete Contreras <elelenacon@gmail.com>
2016-01-28 08:24:24 +0100

    Subiendo cambios del lunes, a Articulo y creado ya el GHibernate

--- GHibernate/src/main/java/Articulo.java
+++ GHibernate/src/main/java/Articulo.java
@@ -1,46 +1,47 @@
 import java.math.BigDecimal;
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+@Entity
 public class Articulo {
-private long id;
-private String nombre;
-private long categoria;
-private BigDecimal precio;
-	public Articulo(){
-		
-	}
-	public Articulo(long id, String nombre, long categoria, BigDecimal precio ){
-		this.id= id;
-		this.nombre = nombre;
-		this.categoria= categoria;
-		this.precio=precio;
-}
-    public long getId() {
+	private Long id;
+	private String nombre;
+	private Long categoria;
+	private BigDecimal precio;
+
+	@Id
+	@GeneratedValue(generator="increment")
+	@GenericGenerator(name="increment", strategy = "increment")
+	public long getId() {
 		return id;
-    }
-
-    public void setId(long id) {
+	}
+	
+	public void setId(long id) {
 		this.id = id;
-    }
-    public String getNombre() {
+	}
+	
+	public String getNombre() {
 		return nombre;
-    }
-
-    public void setNombre(String nombre) {
+	}
+	
+	public void setNombre(String nombre) {
 		this.nombre = nombre;
-    }
-
-    public long getCategoria() {
+	}
+	
+	public long getCategoria() {
 		return categoria;
-    }
-
-    public void setCategoria(long Categoria) {
+	}
+	
+	public void setCategoria(long categoria) {
 		this.categoria = categoria;
-    }
-    
-    public BigDecimal getPrecio(){
-    	return precio;
-    }
-    public void setPrecio(BigDecimal precio){
-    	this.precio= precio;
-    }
+	}
+	
+	public BigDecimal getPrecio() {
+		return precio;
+	}
+	
+	public void setPrecio(BigDecimal precio) {
+		this.precio = precio;
+	}
 }
