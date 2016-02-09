a61a4491978db80c1096b9fb696b98f5be492546
Elena Navarrete Contreras <elelenacon@gmail.com>
2016-02-09 10:16:36 +0100

    Creada la clase cliente

--- GPedido/src/main/java/org/institutoserpis/ad/PruebaPedido.java
+++ GPedido/src/main/java/org/institutoserpis/ad/PruebaPedido.java
@@ -1,10 +1,33 @@
 package org.institutoserpis.ad;
 
-public class PruebaPedido {
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class PruebaPedido {
+	private static EntityManagerFactory entityManagerFactory;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Logger.getLogger("org.hibernate").setLevel(Level.SEVERE);
+		System.out.println("inicio");
+		entityManagerFactory = 
+				Persistence.createEntityManagerFactory("org.institutoserpis.ad");
+		
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		List<Cliente> clientes = entityManager.createQuery("from Cliente", Cliente.class).getResultList();
+			for (Cliente cliente : clientes)
+			System.out.println(cliente);
+			entityManager.getTransaction().commit();
+			entityManager.close();
+		
+		entityManagerFactory.close();
+		System.out.println("fin");
+	}
 
 	}
 
-}
+
